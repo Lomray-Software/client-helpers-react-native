b11c8189cc7610f4dd40c1e1607da09b3ef47957
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2022-10-13 17:42:33 +0200

    fix: remove yup

--- typings/yup.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import type { StringSchema as BaseStringSchema } from 'yup';
-
-declare module 'yup' {
-  interface StringSchema {
-    stripEmptyString(): BaseStringSchema;
-  }
-}
