a96a9b41f7acdbc5c27eda8e342747e18bf8ae60
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2022-11-28 16:28:43 +0100

    fix: ignore some api responses in reactotron

--- src/debug/reactotron-config.ts
+++ src/debug/reactotron-config.ts
@@ -33,7 +33,11 @@ const init = ({ isDebug, AsyncStorage, isTests = false }: IConfig) => {
 
   const reactotron = Reactotron
     .configure() // controls connection & communication settings
-    .useReactNative() // add all built-in react native plugins
+    .useReactNative({
+      networking: {
+        ignoreUrls: /\/(symbolicate|https:\/\/clients3.google.com\/generate_204)$/,
+      }
+    }) // add all built-in react native plugins
     .use(MobxStoreManagerPlugin())
     .connect() // let's connect!
 
