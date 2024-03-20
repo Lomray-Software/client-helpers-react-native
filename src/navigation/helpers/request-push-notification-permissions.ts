7bc466be3ed6bcace42e1a56e0ef31ccd9d533c3
MatthewPattell <mikhail.yarmaliuk@lomray.com>
2024-03-20 11:33:43 +0100

    fix: update firebase

--- src/navigation/helpers/request-push-notification-permissions.ts
+++ src/navigation/helpers/request-push-notification-permissions.ts
@@ -4,26 +4,24 @@ import Config from '../../services/config';
 /**
  * Request IOS Push Notification permissions
  */
-const RequestPushNotificationPermission = (): Promise<boolean> => {
+const RequestPushNotificationPermission = async (): Promise<boolean> => {
   const logger = Config.get('logger');
 
-  return messaging()
-    .requestPermission()
-    .then((authStatus) => {
-      const isEnabled =
-        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-        authStatus === messaging.AuthorizationStatus.PROVISIONAL ||
-        authStatus === messaging.AuthorizationStatus.EPHEMERAL;
+  try {
+    const authStatus = await messaging().requestPermission();
+    const isEnabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL ||
+      authStatus === messaging.AuthorizationStatus.EPHEMERAL;
 
-      logger?.info(`Push permissions authorization status: ${authStatus} (${String(isEnabled)})`);
+    logger?.info(`Push permissions authorization status: ${authStatus} (${String(isEnabled)})`);
 
-      return isEnabled;
-    })
-    .catch((e) => {
-      logger?.error('Request push permissions failed:', e);
+    return isEnabled;
+  } catch (e) {
+    logger?.error('Request push permissions failed:', e);
 
-      return false;
-    });
+    return false;
+  }
 };
 
 export default RequestPushNotificationPermission;
